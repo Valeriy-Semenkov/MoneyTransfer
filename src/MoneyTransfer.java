a6ab814de296f4dca06c34479eb8b31ba9c7cafc
Valeriy Semenkov <unfairlly@gmail.com>
2021-03-14 04:18:18 +0300

    123

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,11 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int incoming = 500_000_000;
+        int count = 2_000_000_000;
+        int total = incoming + count;
+        System.out.println(total);
+
+
+
+    }
+}
